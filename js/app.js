feeead147ab7e346a6f8602d7c2e6a7cc3d1346c
Anders Kofoed <anders.kfd@gmail.com>
2015-02-09 16:24:23 +0100

    added username field + started playing with chrome local storage

--- js/app.js
+++ js/app.js
@@ -1,8 +1,28 @@
-var hcp = angular.module('human-computable-pws', [])
+var hcp = angular.module('human-computable-pws', ['ngAnimate'])
 .controller("MainController", function($scope){
-    $scope.test = {};
-    $scope.test.title = "human computable passwords!";
+  
+
+    $scope.user = {};
+    //chrome.storage.local.get('user', function (result){
+     //   $scope = result;  
+    //});
+
+
+    console.log($scope);
+
+    $scope.updateUser = function () {
+        if ($scope.newUser) {
+            $scope.showForm = false;
+            $scope.user.name = this.newUser ;
+            $scope.newUser = '';
+            saveScope($scope)
+        }
+    }
 });
 
+function saveScope(scope) {
+    chrome.storage.local.set({'user': $scope}, function(){
+        console.log("scope saved");
+    });
 
-        
+}
